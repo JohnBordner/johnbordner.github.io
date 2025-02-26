bf29a30f3d4ace0353f18e32c0fcff4fd72b13a7
JohnBordner <155011654+JohnBordner@users.noreply.github.com>
2025-02-26 14:17:34 -0600

    Stop tracking dist/ (ignored in .gitignore)

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import React from "react";
-import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
-import DashPage from "./pages/DashPage";
-import Home from "./pages/Home";
 const router = createBrowserRouter([
   {
     path: "/",
